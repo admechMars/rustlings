31f9a582634219474c946ac00e2ad76ee163e6c8
Luis <baskerbyte@gmail.com>
2024-06-14 06:45:13 -0400

    ops

--- exercicios/01_variaveis/variaveis_quiz.rs
+++ exercicios/01_variaveis/variaveis_quiz.rs
@@ -4,9 +4,18 @@
 
 // EU NÃO ESTOU PRONTO
 
-fn meus_dados() -> (&str, i32, f64) {
+fn meus_dados() -> (&'static str, i32, f64) {
     // crie 3 variáveis com o seu `nome`, `idade` e `altura`
+
+
 
     // Não modificar a linha abaixo
     (nome, idade, altura)
 }
+
+
+
+
+// Ignorar as linhas a baixo
+#[cfg(test)]
+mod tests {}
